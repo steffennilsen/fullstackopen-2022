79534c31844b1c5b808f3cb27240b5600eac077c
Steffen Nilsen <steffennilsen@gmail.com>
2022-09-06 15:48:19 +0200

    phonebook step 4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -5,8 +5,7 @@ const PhonebookForm = ({ name, persons, number }) => {
     event.preventDefault();
 
     /**
-     * purposedly not doing .toLowerCase or similiar since its not specified
-     * nor filtering on existing numbers, since its not specified
+     * purposedly not filtering on existing numbers, since its not specified
      */
     if (persons.get.map((person) => person.name).includes(name.get)) {
       window.alert(`${name.get} is already added to phonebook`);
@@ -15,8 +14,12 @@ const PhonebookForm = ({ name, persons, number }) => {
 
     /**
      * numbers set as strings due to format issues like dashes, and its not specified
+     * `id: persons.get.length + 1` because dummy data is 1 indexed 🤮🤮🤮
      */
-    persons.set([...persons.get, { name: name.get, number: number.get }]);
+    persons.set([
+      ...persons.get,
+      { name: name.get, number: number.get, id: persons.get.length + 1 },
+    ]);
     name.set("");
     number.set("");
   };
@@ -25,14 +28,14 @@ const PhonebookForm = ({ name, persons, number }) => {
     <form onSubmit={addEntry}>
       <div>
         <div>
-          name:
+          <label>name:</label>
           <input
             value={name.get}
             onChange={(event) => name.set(event.target.value)}
           />
         </div>
         <div>
-          number:
+          <label>number:</label>
           <input
             value={number.get}
             onChange={(event) => number.set(event.target.value)}
@@ -50,7 +53,8 @@ const NumberList = ({ persons }) => (
   <table>
     <tbody>
       {persons.map((person) => (
-        <tr key={person.name}>
+        <tr key={person.id}>
+          <td>{person.id}</td>
           <td>{person.name}</td>
           <td>{person.number}</td>
         </tr>
@@ -59,37 +63,48 @@ const NumberList = ({ persons }) => (
   </table>
 );
 
-function randomPhonenumber() {
-  const nr = `${Math.ceil(Math.random() * 10_000_000_000) - 1}`;
-  return `${nr.substring(0, 3)}-${nr.substring(3, nr.length)}`;
-}
-
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      name: "Arto Hellas",
-      number: randomPhonenumber(),
-    },
-    {
-      name: "Ada Lovelace",
-      number: randomPhonenumber(),
-    },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
+
+  const filterPersons = () =>
+    filter.length === 0
+      ? persons
+      : persons.filter((person) =>
+          person.name.toLowerCase().includes(filter.toLowerCase())
+        );
 
   return (
     <div>
-      <h2>Phonebook</h2>
-      {/* Really should wait with this until event propogation gets introduced? */}
-      <PhonebookForm
-        persons={{ get: persons, set: setPersons }}
-        name={{ get: newName, set: setNewName }}
-        number={{ get: newNumber, set: setNewNumber }}
-      />
+      <div>
+        <h2>Phonebook</h2>
+        <div>
+          <label>filter shown with</label>
+          <input
+            value={filter}
+            onChange={(event) => setFilter(event.target.value)}
+          />
+        </div>
+      </div>
+      <div>
+        <h2>add a new</h2>
+        {/* Really should wait with this until event propogation gets introduced? */}
+        <PhonebookForm
+          persons={{ get: persons, set: setPersons }}
+          name={{ get: newName, set: setNewName }}
+          number={{ get: newNumber, set: setNewNumber }}
+        />
+      </div>
       <div>
         <h2>Numbers</h2>
-        <NumberList persons={persons} />
+        <NumberList persons={filterPersons()} />
       </div>
     </div>
   );
