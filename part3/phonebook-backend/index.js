6d62ee7c464992aac55cdbc716819a5dfb5351c6
Steffen Nilsen <steffennilsen@gmail.com>
2022-09-13 12:18:10 +0200

    phonebook database step 3,4,5,6

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -5,145 +5,221 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const morgan = require('morgan');
 const chalk = require('chalk');
+const httpError = require('http-errors');
 const Person = require('./models/person');
 
 const chalkSuccess = chalk.green;
+const chalkInfo = chalk.bgBlue.white;
 const chalkWarn = chalk.bgYellow.black;
 const chalkError = chalk.bold.red;
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const URL = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+mongoose
+  .connect(MONGODB_URI)
+  .then((result) =>
+    console.log(chalkSuccess(`connected to ${result.connections[0].host}`)),
+  )
+  .catch((error) => {
+    console.error(chalkError('error connecting to MongoDB'));
+    console.error(chalkError(error.message));
+    process.exit(1);
+  });
+
+/**
+ * Middleware
+ */
+
+const enforceJSONContentType = (req, res, next) => {
+  if (
+    ['POST', 'PUT', 'PATCH'].find((method) => method === req.method) &&
+    req.get('Content-Type') !== 'application/json'
+  ) {
+    return res.sendStatus(415);
+  }
+
+  return next();
+};
+
+// https://stackoverflow.com/a/58165719/2029532
+const jsonParseError = (err, req, res, next) => {
+  if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+    console.warn(chalkWarn(err.message));
+    console.warn(chalkWarn(err.body));
+    return res.status(400).send({ error: err.message });
+  }
+  return next();
+};
+
+/**
+ * Middleware end
+ */
 
 app.use(
   express.static('build'),
   morgan.token('body', (req, res) => JSON.stringify(req.body))(
     ':method :url :status :res[content-length] - :response-time ms :body',
   ),
-  cors(),
+  enforceJSONContentType,
   express.json(),
+  jsonParseError,
+  cors(),
 );
 
-mongoose
-  .connect(URL)
-  .then((result) =>
-    console.log(chalkSuccess(`connected to ${result.connections[0].host}`)),
-  )
-  .catch((error) =>
-    console.error(chalkError('error connecting to MongoDB:', error.message)),
-  );
+/**
+ * /info
+ */
 
-app.get('/info', async (request, response) => {
-  const count = await Person.estimatedDocumentCount();
-  return response.send(
+app.get('/info', async (req, res, next) => {
+  const count = await Person.estimatedDocumentCount().catch((error) =>
+    next(error),
+  );
+  return res.send(
     `<div><p>Phonebook has info for ${count} people</p><p>${Date()}</p></div>`,
   );
 });
-app.all('/info', (request, response) => response.status(405).end());
 
-app.get('/api/persons', async (request, response) => {
-  const persons = await Person.find({});
-  return response.json(persons);
+/**
+ * /api/persons
+ */
+
+app.get('/api/persons', async (req, res, next) => {
+  const persons = await Person.find({}).catch((error) => next(error));
+  return res.json(persons);
 });
-app.post('/api/persons', async (request, response) => {
-  if (request.get('Content-Type') !== 'application/json') {
-    return response.status(400).json({
-      error: 'expect Content-Type: application/json',
-    });
+
+app.post('/api/persons', async (req, res, next) => {
+  const invalid = invalidPersonEntries(req.body, res);
+  if (!!invalid) {
+    return invalid;
   }
 
-  const existingPerson = await Person.findOne({ name: request.body.name });
-  if (existingPerson !== null) {
-    console.log(
-      chalkWarn(`POST /api/persons rejected, name exists`),
-      chalkWarn(JSON.stringify(existingPerson.name)),
-    );
-    return response.status(409).json({
+  const exists = await checkIfPersonExists(req.body.name, res, next);
+  if (!!exists) {
+    return exists;
+  }
+
+  const person = await new Person({
+    name: req.body.name,
+    number: `${req.body.number}`,
+  })
+    .save()
+    .catch((err) => next(err));
+
+  console.log(chalkSuccess(`saved ${JSON.stringify(person)}`));
+  return res.status(201).json(person);
+});
+
+/**
+ * /api/persons/:id
+ */
+
+app.get('/api/persons/:id', async (req, res, next) => {
+  const person = await Person.findById(req.params.id).catch((err) => next(err));
+  if (person === null) {
+    return res.sendStatus(404);
+  }
+
+  return res.json(person);
+});
+
+app.delete('/api/persons/:id', async (req, res, next) => {
+  const person = await Person.findByIdAndRemove(req.params.id).catch((err) =>
+    next(err),
+  );
+  if (person === null) {
+    return res.sendStatus(404);
+  }
+
+  return res.json(person);
+});
+
+app.put('/api/persons/:id', async (req, res, next) => {
+  const invalid = invalidPersonEntries(req.body, res);
+  if (!!invalid) {
+    return invalid;
+  }
+
+  const entries = { name: req.body.name, number: req.body.number };
+  const person = await Person.findByIdAndUpdate(req.params.id, entries, {
+    new: true,
+    upsert: false,
+  }).catch((err) => next(err));
+
+  if (person === null) {
+    return res.status(404).send({ error: 'id not found' });
+  }
+
+  console.log(chalkSuccess(`updated ${JSON.stringify(person)}`));
+  return res.json(person);
+});
+
+/**
+ * Route end
+ */
+
+/**
+ * 404 is not an actual error, so lets make it one
+ * so the endpointhandler has something to work with
+ */
+app.use((req, res, next) => next(new httpError(404)));
+
+/**
+ * Handle malformed id's
+ */
+app.use((err, req, res, next) => {
+  if (err.name === 'CastError' && err.kind === 'ObjectId') {
+    return res.status(400).send({ error: 'malformatted id' });
+  }
+
+  return next(err);
+});
+
+/**
+ * Handle unknown endpoint
+ */
+app.use((err, req, res, next) => {
+  console.warn(chalkWarn(`unknownEndpoint`));
+  return res.status(404).send({ error: 'unknown endpoint' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+/**
+ * Utils
+ */
+
+/**
+ * @returns `router response` if exists, `false` if not
+ */
+async function checkIfPersonExists(name, res, next) {
+  const person = await Person.findOne({
+    name,
+  }).catch((err) => next(err));
+
+  if (person !== null) {
+    console.warn(chalkWarn(`${person.name} already exists`));
+    return res.status(409).json({
       error: 'name must be unique',
     });
   }
 
-  const person = new Person({
-    name: request.body.name,
-    number: `${request.body.number}`,
-  });
-
-  return person
-    .save()
-    .then((result) => {
-      console.log(chalkSuccess(`saved ${JSON.stringify(result)}`));
-      return response.status(201).json(result);
-    })
-    .catch((error) => {
-      console.warn(chalkWarn(JSON.stringify(person)));
-      return response.status(400).json({ error: error });
-    });
-});
-app.all('/api/persons', (request, response) => response.status(405).end());
-
-app.get('/api/persons/:id', async (request, response) => {
-  return Person.findById(request.params.id)
-    .then((person) => {
-      if (person.statusCode) {
-        return response.status(statusCode).end();
-      }
-
-      return response.json(person);
-    })
-    .catch((error) => {
-      const idCastError = checkIdCastError(error);
-      if (!!idCastError) {
-        return response.status(400).json({ error: idCastError });
-      }
-
-      return response.status(404).end();
-    });
-});
-app.delete('/api/persons/:id', async (request, response) => {
-  return Person.findOneAndRemove({ _id: request.params.id })
-    .then((deleted) => {
-      if (!!deleted) {
-        console.warn(chalkWarn(`deleted ${request.params.id}`));
-        return response.status(204).end();
-      }
-
-      return response.status(404).end();
-    })
-    .catch((error) => {
-      const idCastError = checkIdCastError(error);
-      if (!!idCastError) {
-        return response.status(400).json({ error: idCastError });
-      }
-
-      console.error(chalkError(error));
-      return response.json(error);
-    });
-});
+  return false;
+}
 
 /**
- * Almost tripped me when I tried PUT, postponing implementation
- *
- * 3.9 phonebook backend step9
- * Make the backend work with the phonebook frontend from the exercises of the previous part.
- * Do not implement the functionality for making changes to the phone numbers yet, that will be implemented in exercise 3.17.
+ * @returns `router response` invalid, `false` if not
  */
-app.put('/api/persons/:id', (request, response) => response.status(501).end());
-app.all('/api/persons/:id', (request, response) => response.status(405).end());
-
-app.use((request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' });
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`, URL);
-});
-
-function checkIdCastError(error) {
-  if (error.name === 'CastError' && error.kind === 'ObjectId') {
-    const msg =
-      'id must be a string of 12 bytes or a string of 24 hex characters or an integer';
-    console.error(chalkError(msg));
-    return msg;
+function invalidPersonEntries(person, res) {
+  for (const key of ['name', 'number']) {
+    if (person[key] === undefined || person[key] === null) {
+      console.warn(chalkWarn(`invalid or missing entries`));
+      return res.status(400).json({ error: 'invalid or missing entries' });
+    }
   }
 
   return false;
