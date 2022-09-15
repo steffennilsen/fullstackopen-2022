2ac776b8d98cb210a8e396e1849568f29d004cd0
Steffen Nilsen <steffennilsen@gmail.com>
2022-09-15 13:25:33 +0200

    silenced loggers during testing

--- part4/blog-backend/utils/logger.js
+++ part4/blog-backend/utils/logger.js
@@ -1,26 +1,30 @@
 /* eslint-disable no-console */
 const chalk = require('chalk');
+const { NODE_ENV } = require('./config');
 
-const log = (...params) => {
-  console.log(...params);
-};
-
-const success = (...params) => {
-  console.log(chalk.green(...params));
-};
-
-const info = (...params) => {
-  console.log(chalk.bgBlue.white(...params));
-};
-
-const warn = (...params) => {
-  console.log(chalk.bgYellow.black(...params));
-};
-
-const error = (...params) => {
-  console.error(chalk.bold.red(...params));
-};
+const log = (...params) => console.log(...params);
+const success = (...params) => console.log(chalk.green(...params));
+const info = (...params) => console.log(chalk.bgBlue.white(...params));
+const warn = (...params) => console.log(chalk.bgYellow.black(...params));
+const error = (...params) => console.error(chalk.bold.red(...params));
 
 module.exports = {
-  log, success, info, warn, error,
+  log,
+  success,
+  info,
+  warn,
+  error,
 };
+
+if (NODE_ENV === 'test') {
+  module.exports = {
+    ...module.exports,
+    ...{
+      log: () => {},
+      success: () => {},
+      info: () => {},
+      warn: () => {},
+      error: () => {},
+    },
+  };
+}
